e42bb8590f87c2aaafe3064978059cfe911efa93
shuangliang kolu <shuangkolu@hotmail.com>
2016-06-22 11:42:56 +0300

    JS. Navbar.js Comments and some bug fixes.

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,107 +1,197 @@
+// TODO move fire event to personal namespace for functions
+
 var ResponsiveNavbar = function () {
-    // Toggle display to none/inline when users press collapser links inside navbar
+    
+    // Some popular document elements that'll be used a lot.
     var collapser = document.getElementsByClassName('collapser');
-    var nested = document.getElementsByClassName('navbar-nested');
     var ulList = document.getElementsByClassName('nav-links');
     var navbar_container = document.getElementById('navbar-container');
+    
+    // Hide navbar's scrollbar if there is any. If not padding-left will be 0px
     navbar_container.style.paddingLeft = (navbar_container.offsetWidth - navbar_container.clientWidth) + 'px';
+    
+    // Boolean variables
     var collapsed = false;
     var mobile_view = false;
+    
+    // Current open collapser.
+    // If none is open, then this value will be null.
     var selected_nested = null;
-    // Check if styles are already in mobile view
+    
+    // Check if browser is already in mobile view.
     if (document.documentElement.clientWidth < 768) {
         mobile_view = true;
     }
-    // Adds events listener that collapsed nested links to dropdown links in navbar.
+
+    // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+    // 
+    // NavBar elements eventlisteners
+    // 1. navbar nested collapser: show/hide nested links inside collapser
+    // 2. Navbar compact view "hamburger": show/hide navbar when it is in compact/mobile view
+    //
+    // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+
+    // 1
+    // Loops through all collapser and adds eventlistener to them.
+    //
+    // Prevents elements default action and stops propagation.
+    //
+    // Sets this object "selected_nested" variable.
+    //
+    // useCapture: false
+    //
     for (let i = 0; i < collapser.length; i++) {
         collapser[i].addEventListener('click', function (event) {
             event.preventDefault();
             event.stopPropagation();
-            var el = this.parentElement.getElementsByClassName('navbar-nested')[0];
-            if (el.style.display === 'inline') {
-                el.style.display = 'none';
+
+            // Get ul elements that contains nested links.
+            // Collapser and said ul are both under same parent.
+            var elem = this.parentElement.querySelector('.navbar-nested');
+
+            // Hides links if they are already displayed ( have css style "display: inline" )
+            // Also resets selected_nested 
+            if (elem.style.display === 'inline') {
+                elem.style.display = 'none';
                 selected_nested = null;
             }
+            // If links are not displayed, then display them.
             else {
-                // Hide all other displayed navbar elements.
-                // There should be only 0 or 1 displayed navbar-nested,
-                // so breaks away after hiding first one.
-                for (var i = 0; i < collapser.length; i++) {
-                    if (nested[i].style.display === 'inline') {
-                        nested[i].style.display = 'none';
-                        break;
+                // If there is another collapser links visible then hides them.
+                if(selected_nested != null){
+                    let temp = selected_nested.parentElement.querySelector('.navbar-nested');
+                    if (temp.style.display === 'inline') {
+                        temp.style.display = 'none';
+                        selected_nested = null;
                     }
                 }
-                el.style.display = 'inline';
+                // Display links.
+                // Also save this as selected
+                elem.style.display = 'inline';
                 selected_nested = this;
             }
         }, false);
     }
 
-    // Click event for "hamburger" that's only visible on lower widths.
-    // Display navbar links depending on if it's currently collapsed or not.
+    // 2
+    // Compact view "hamburger" collapser
+    //
+    // Navbar links ( except brand/logo ) will be hidden in compact/mobile view.
+    // This eventlistener will show/hide links inside navbar.
+    //
+    // Prevents default action and propagation from click.
+    //
+    //  Sets this object "collapsed" variable.
+    //
+    // useCapture: false
+    //
     document.getElementById('navbar-collapser').addEventListener('click', function (event) {
         event.preventDefault();
         event.stopPropagation();
-        let i;
-        for (i = 0; i < ulList.length; i++) {
-            if (collapsed) {
+        // Loops through all link containers ( ul ) inside navbar
+        // Change navbar links visibility depending on current status ( collapsed )
+        if (collapsed) {
+            for (let i = 0; i < ulList.length; i++) {
                 ulList[i].style.display = 'none';
             }
-            else {
-                ulList[i].style.display = 'inline';
-            }
-        }
-
-        if (collapsed) {
             collapsed = false;
         }
         else {
-            collapsed = true;
-        }
-        if (selected_nested != null) {
-            eventFire(selected_nested, 'click');
-        }
-    }, false);
-    // Changes display.style for navbar links on entering and existing mobile views.
-    // Use eventlistener for window resize
-    // Css don't seems to override JavaScript changes. So this seems to be easiest way.
-    // Use mobile_view so actions are only performed once after entering/existing.
-    window.addEventListener('resize', function () {
-        if (window.innerWidth >= 769 && mobile_view === true) {
             for (let i = 0; i < ulList.length; i++) {
                 ulList[i].style.display = 'inline';
             }
-            navbar_container.style.paddingLeft = (navbar_container.offsetWidth - navbar_container.clientWidth) + 'px';
-            mobile_view = false;
+            collapsed = true;
         }
-        else if (window.innerWidth <= 768 && mobile_view === false) {
-            for (let i = 0; i < ulList.length; i++) {
-                ulList[i].style.display = 'none';
-                collapsed = false;
-            }
-            navbar_container.style.paddingLeft = (navbar_container.offsetWidth - navbar_container.clientWidth) + 'px';
-            mobile_view = true;
-        }
+        // Close collapsed nested link
         if (selected_nested != null) {
             eventFire(selected_nested, 'click');
         }
+    }, false);
+
+    // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+    // 
+    // Browser element eventlisteners.
+    // 1. window resize: for navbar compact view
+    // 2. click on any DOM element: for closing navbar
+    //
+    // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+
+    // 1
+    //
+    // Changes display.style for navbar links on entering and existing mobile views.
+    //
+    // Use eventlistener for window resize
+    //
+    // Css don't seems to override JavaScript changes. So this seems to be easiest way.
+    // Use mobile_view so actions are only performed once after entering/existing.
+    //
+    window.addEventListener('resize', function () {
+        // Checks for entering mobile or normal view.
+        if (window.innerWidth >= 769 && mobile_view === true) {
+            // Show all links except those nested in normal view.
+            for (let i = 0; i < ulList.length; i++) {
+                ulList[i].style.display = 'inline';
+            }
+            collapsed = false;
+            mobile_view = false;
+            // Remove padding that hides scrollbar on navbar.
+            navbar_container.style.paddingLeft = (navbar_container.offsetWidth - navbar_container.clientWidth) + 'px';
+            // Close displayed nested links if there is any.
+            if (selected_nested != null) { eventFire(selected_nested, 'click'); }
+        }
+        else if (window.innerWidth <= 768 && mobile_view === false) {
+            // Hide all links in mobile view.
+            for (let i = 0; i < ulList.length; i++) {
+                ulList[i].style.display = 'none';
+            }
+            collapsed = false;
+            mobile_view = true;
+            // Add padding that hides scrollbar on navbar
+            navbar_container.style.paddingLeft = (navbar_container.offsetWidth - navbar_container.clientWidth) + 'px';
+            // Close displayed nested links if there is any.
+            if (selected_nested != null) { eventFire(selected_nested, 'click'); }
+        }
     });
 
-    // Close displayed navbar-nested if user clicks on any elements
-    // other than navbar-nested collapser
+    // 2
+    //
+    // Closes navbar and collapsed nested links when user interact with other part of page.
+    //
     document.addEventListener('click', function () {
-        for (let i = 0; i < collapser.length; i++) {
-            if (nested[i].style.display === 'inline') {
-                nested[i].style.display = 'none';
-                break;
+        // Check if theres any collapsed nested links
+        if(selected_nested != null){
+            // Close them if there are
+            let temp = selected_nested.parentElement.querySelector('.navbar-nested');
+            if (temp.style.display === 'inline') {
+                temp.style.display = 'none';
+                selected_nested = null;
             }
         }
-        if (collapsed) {
+        // Check if navbar is collapsed and in mobile view.
+        // 1. resize eventlistener will close navbar when user enters normal view from mobile view.
+        if (collapsed && mobile_view) {
             eventFire(document.getElementById('navbar-collapser'), 'click');
         }
     }, false);
-    // Fire eventType on element.
+
+    
+    // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+    // 
+    // Misc functions
+    // 1. fireEvent(element, eventType) - fire event on element
+    //
+    // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+    
+    // 1
+    // Fires parameter 2 "eventType" on parameter 1 "element"
+    //
+    // IE browser uses fireEvent
+    // Most other browses uses W3C dispatchEvent
+    //
+    // canBubbleArg: true
+    // cancelableArg: false
+    // TODO: write version with arguments when needed...
+    //
     function eventFire(element, eventType) {
         if (element.fireEvent) {
             element.fireEvent('on' + eventType);
